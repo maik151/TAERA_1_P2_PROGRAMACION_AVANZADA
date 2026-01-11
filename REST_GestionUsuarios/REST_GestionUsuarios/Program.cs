1278af14deff8ba13abdea5f3ed09b575a2f2364
maicol_jb <64624079+maik151@users.noreply.github.com>
2026-01-10 20:57:46 -0500

    updload program.cs

--- REST_GestionUsuarios/REST_GestionUsuarios/Program.cs
+++ REST_GestionUsuarios/REST_GestionUsuarios/Program.cs
@@ -3,41 +3,47 @@ using REST_GestionUsuarios.Infrastructure.Data;
 using REST_GestionUsuarios.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --- SERVICIOS ---
 builder.Services.AddSingleton<IDbConnectionFactory, OracleConnectionFactory>();
-
-
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 
+// --- CORS (Configuración permisiva para producción) ---
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.AllowAnyOrigin()   
-              .AllowAnyHeader()   
-              .AllowAnyMethod(); 
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
 builder.Services.AddControllers();
-
-// Configuración de Swagger / OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// --- CONFIGURACIÓN ORACLE WALLET (RUTAS DINÁMICAS) ---
+string walletPath = Path.Combine(Directory.GetCurrentDirectory(), "Infraestructure", "OracleWallet");
+Oracle.ManagedDataAccess.Client.OracleConfiguration.TnsAdmin = walletPath;
+Oracle.ManagedDataAccess.Client.OracleConfiguration.WalletLocation = walletPath;
+
 var app = builder.Build();
+
+// --- MIDDLEWARES ---
 app.UseSwagger();
 app.UseSwaggerUI(c => {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = "swagger"; // Esto asegura que la ruta sea /swagger
+    c.RoutePrefix = "swagger";
 });
 
-app.UseHttpsRedirection();
+// Comentado para evitar errores 400 en Render
+// app.UseHttpsRedirection(); 
 
 app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 app.MapControllers();
-
 
 app.Run();
